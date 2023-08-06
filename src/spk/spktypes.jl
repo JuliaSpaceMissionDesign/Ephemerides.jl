30117be83c7d3e5fab88a2593019c2b50e4c7147
MicheleCeresoli <michele.ceresoli7@gmail.com>
2023-08-07 00:14:05 +0200

    Added documentation

--- src/spk/spktypes.jl
+++ src/spk/spktypes.jl
@@ -29,11 +29,26 @@ abstract type AbstractSPKCache end
 # SPK TYPE 1
 # ----------------------------------
 
+"""
+    SPKSegmentHeader1 <: AbstractSPKHeader
+
+Header instance for SPK segments of type 1. 
+
+### Fields 
+- `n` -- `Int` number of records in the segment 
+- `ndirs` -- `Int` number of directory epochs
+"""
 struct SPKSegmentHeader1 <: AbstractSPKHeader 
     n::Int      # number of records in the segment
     ndirs::Int 
 end
 
+""" 
+    SPKSegmentCache1 <: AbstractSPKCache 
+
+Cache instance for SPK segments of type 1. The fields contained within this cache 
+are taken from the FORTRAN NAIF's SPICE implementation for type 1 SPK segments. 
+"""
 struct SPKSegmentCache1 <: AbstractSPKCache
     
     tl::Vector{Float64}
@@ -49,7 +64,22 @@ struct SPKSegmentCache1 <: AbstractSPKCache
 
 end
 
-struct SPKSegmentType1
+""" 
+    SPKSegmentType1 <: AbstractSPKSegment
+
+Segment instance for SPK segments of type 1, which contain Modified Difference Arrays (MDA). 
+This data type is normally used for spacecraft whose ephemerides are produced by JPL's 
+principal trajectory integrator DPTRAJ. 
+
+### Fields 
+- `head` -- Segment header 
+- `cache` -- Segment cache 
+
+### References 
+- [SPK Required Reading](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/spk.html)
+- [SPICE Toolkit](https://naif.jpl.nasa.gov/naif/toolkit_FORTRAN.html)
+"""
+struct SPKSegmentType1 <: AbstractSPKSegment
     head::SPKSegmentHeader1
     cache::SPKSegmentCache1
 end
@@ -59,29 +89,80 @@ end
 # SPK TYPE 2
 # ----------------------------------
 # TODO: add number of components to be able to handle TYPE 3 aswell with same header! 
+
+"""
+    SPKSegmentHeader2 <: AbstractSPKHeader
+
+Header instance for SPK segments of type 2.
+
+### Fields 
+- `tstart` -- `Float64` initial epoch of the first record, in seconds since J2000
+- `tlen` -- `Float64` interval length covered by each record, in seconds
+- `order` -- `Int` polynomial order 
+- `n` -- `Int` number of records in the segment
+"""
 struct SPKSegmentHeader2 <: AbstractSPKHeader
-    tstart::Float64     # segment starting epoch 
-    tlen::Float64       # interval length 
-    order::Int          # polynomial order (retrieved from array size)
-    n::Int              # number of records in segment 
+    tstart::Float64     
+    tlen::Float64       
+    order::Int
+    n::Int              
 end
 
+""" 
+    SPKSegmentCache2 <: AbstractSPKCache 
+
+Cache instance for SPK segments of type 2.
+
+### Fields 
+- `A` -- Chebyshev's polynomial coefficients, with size (ncomp, order)
+- `x` -- Values of the Chebyshev's polynomials
+"""
 struct SPKSegmentCache2 <: AbstractSPKCache
     A::Matrix{Float64}
     x::Vector{Float64}
 end
 
+""" 
+    SPKSegmentType2 <: AbstractSPKSegment
+
+Segment instance for SPK segments of type 2, which contain Chebyshev polynomial coefficients 
+for the position and/or state of the body as function of time. This data type is normally 
+used for planet barycenters, and for satellites whose ephemerides are integrated.
+
+### Fields 
+- `head` -- Segment header 
+- `cache` -- Segment cache 
+
+### References 
+- [SPK Required Reading](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/spk.html)
+- [SPICE Toolkit](https://naif.jpl.nasa.gov/naif/toolkit_FORTRAN.html)
+"""
 struct SPKSegmentType2 <: AbstractSPKSegment
     head::SPKSegmentHeader2
     cache::SPKSegmentCache2
 end
 
 
-
 # ----------------------------------
 # SPK SEGMENT LIST
 # ----------------------------------
 
+"""
+    SPKSegmentList 
+
+A container object to efficiently store all the different SPK segments that are contained 
+within a single DAF file.
+
+---
+
+    SPKSegmentList()
+
+Initialises an empty `SPKSegmentList` object.
+
+### See also 
+See also [`add_segment!`](@ref)
+
+"""
 struct SPKSegmentList
     
     spk1::Vector{SPKSegmentType1}
