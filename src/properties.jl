ad3b37a07254400db393bd6a160e94ff15e5c275
MicheleCeresoli <michele.ceresoli7@gmail.com>
2023-08-08 16:41:52 +0200

    Added properties documentation

--- src/properties.jl
+++ src/properties.jl
@@ -78,11 +78,16 @@ ephem_available_axes(eph::EphemerisProvider) = Int.(keys(pck_links(eph)))
 
 """
     AbstractEphemRecord
+
+Abstract type for ephemeris segment records.
 """
 abstract type AbstractEphemRecord end 
 
 """
     initial_times(record::AbstractEphemRecord)
+
+Recover the initial times of each sub-window in which the ephemeris data of `record` is 
+defined, expressed in TDB seconds since J2000
 """
 function initial_times(::T) where {T <: AbstractEphemRecord}
     throw(ErrorException("`initial_times` shall be implemented for type $T."))
@@ -90,6 +95,9 @@ end
 
 """
     final_times(record::AbstractEphemRecord)
+
+Recover the final times of each sub-window in which the ephemeris data of `record` is 
+defined, expressed in TDB seconds since J2000
 """
 function final_times(::T) where {T <: AbstractEphemRecord}
     throw(ErrorException("`final_times` shall be implemented for type $T."))
@@ -101,11 +109,11 @@ end
 Store the SPK metadata relative to a given (target, center) objects pair.
 
 ### Fields 
-- `target` -- 
-- `center` --
-- `axes` --
-- `t_start` --
-- `t_end` --
+- `target` -- `Int` target object NAIF ID
+- `center` -- `Int` center object NAIF ID
+- `axes` -- `Int` reference axes ID 
+- `t_start` -- start times of each sub-window, in TDB seconds since J2000
+- `t_end` -- final times of each sub-window, in TDB seconds since J2000
 
 """
 struct EphemRecordSPK <: AbstractEphemRecord
@@ -123,6 +131,8 @@ end
 """
     ephem_spk_records(eph::EphemerisProvider)
 
+Return a list of `EphemRecordSPK` storing metadata relative to each (target, center)
+object pairs in the loaded SPK kernels. 
 """
 function ephem_spk_records(eph::EphemerisProvider)
     
@@ -166,10 +176,10 @@ end
 Store the PCK metadata relative to a given (target, center) axes pair.
 
 ### Fields 
-- `target` -- 
-- `center` --
-- `t_start` --
-- `t_end` --
+- `target` -- `Int` target axes NAIF ID
+- `center` -- `Int` center axes NAIF ID
+- `t_start` -- start times of each sub-window, in TDB seconds since J2000
+- `t_end` -- final times of each sub-window, in TDB seconds since J2000
 """
 struct EphemRecordPCK <: AbstractEphemRecord
     target::Int 
@@ -185,6 +195,8 @@ end
 """
     ephem_pck_records(eph::EphemerisProvider)
 
+Return a list of `EphemRecordPCK` storing metadata relative to each (target, center)
+axes pairs in the loaded PCK kernels. 
 """
 function ephem_pck_records(eph::EphemerisProvider)
 
@@ -214,6 +226,10 @@ end
 
 """
     get_segment_boundaries(desclist::Vector{DAFSegmentDescriptor})
+
+Parse all the segment descriptors of a given (center, target) pair and return a set of 
+initial and final times, in TDB seconds since J2000, representing all the time 
+sub-windows in which the ephemeris data for this pair is defined. 
 """
 function get_segment_boundaries(desclist::Vector{DAFSegmentDescriptor})
 
@@ -283,29 +299,72 @@ function get_segment_boundaries(desclist::Vector{DAFSegmentDescriptor})
 
         end
 
+        # if a == b the segment is discarded because 
+        # its time interval is already covered.
+
     end
 
     return t_start, t_end
 
 end
 
-
 """
     ephem_spk_timespan(eph::EphemerisProvider)
+
+Return the minimum and maximum time available in the SPK kernels loaded within `eph`, in 
+TDB seconds since J2000, together with a continuity parameter defined as follows: 
+
+- **0** no SPK data is available.
+- **1** the quantities of all bodies are available for any time between the first and last time.
+- **2** the quantities of some bodies are available on discontinuous time intervals between the 
+    first and last time.
+- **3** the quantities of each body are available on a continuous time interval between the first 
+    and the last time, but not available for any time between the first and last time.
+
+
+### References 
+- [CALCEPH](https://www.imcce.fr/inpop/calceph) C++ library
+
+### See Also 
+See also [`ephem_pck_timespan`](@ref).
+
 """
 ephem_spk_timespan(eph::EphemerisProvider) = analyse_timespan(ephem_spk_records(eph))
 
-
 """
     ephem_pck_timespan(eph::EphemerisProvider)
+
+Return the minimum and maximum time available in the PCK kernels loaded within `eph`, in 
+TDB seconds since J2000, together with a continuity parameter defined as follows: 
+
+- **0** no PCK data is available.
+- **1** the quantities of all axes are available for any time between the first and last time.
+- **2** the quantities of some axes are available on discontinuous time intervals between the 
+    first and last time.
+- **3** the quantities of each axis are available on a continuous time interval between the first 
+    and the last time, but not available for any time between the first and last time.
+
+### References 
+- [CALCEPH](https://www.imcce.fr/inpop/calceph) C++ library
+
+### See Also 
+See also [`ephem_spk_timespan`](@ref).
 """
 ephem_pck_timespan(eph::EphemerisProvider) = analyse_timespan(ephem_pck_records(eph))
 
-
 """
     analyse_timespan(records)
+
+Analyse a set of [`AbstractEphemRecord`](@ref), returning the minimum and maximum covered 
+times, in TDB seconds since J2000, together with a continuity parameter.
+
+### References 
+- [CALCEPH](https://www.imcce.fr/inpop/calceph) C++ library
+
+### See Also 
+See also [`ephem_spk_timespan`](@ref) and [`ephem_pck_timespan`](@ref).
 """
-function analyse_timespan(records)
+function analyse_timespan(records::Vector{<:AbstractEphemRecord})
     firsttime, lasttime = Inf, -Inf
     continuity = 0
 
