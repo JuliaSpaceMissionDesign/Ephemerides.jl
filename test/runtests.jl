ae12edd3d4b041912c8f2e8b44e812e38feafa64
MicheleCeresoli <michele.ceresoli7@gmail.com>
2023-08-22 18:31:39 +0200

    Added artifacts

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,16 @@ using Ephemerides
 using Test 
 
 using CalcephEphemeris
-using ForwardDiff 
-using SPICE 
-using Tempo 
-
-using JSMDInterfaces.Ephemeris
 using JSMDUtils.Math: D¹, D², D³
+using LazyArtifacts
+
+import JSMDInterfaces.Ephemeris as jEphem
+
+@testset "Download all artifacts" begin
+    @info artifact"testdata"
+    @info "All artifacts downloaded"
+end;
+
 
 @testset "Ephemerides" verbose=true begin 
     
