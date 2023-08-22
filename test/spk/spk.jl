3727dc777638b4540ba7308bbf7c91a7b97e8f0c
MicheleCeresoli <michele.ceresoli7@gmail.com>
2023-08-22 19:11:39 +0200

    Added SPK Type 2 tests

--- test/spk/spk.jl
+++ test/spk/spk.jl
@@ -2,7 +2,7 @@
 @testset "SPK Segments" verbose=true begin 
 
     include("spk1.jl")
-    # include("spk2.jl")
+    include("spk2.jl")
     # include("spk8.jl")
     # include("spk12.jl")
     # include("spk21.jl")
